c01c95f79e4d502d2a46db24f5a4156fe3874b19
JasonMAdelmann <JasonMAdelmann>
2016-11-12 15:53:07 -0800

    11/12 Chgs

--- APM - Start/app/app.module.ts
+++ APM - Start/app/app.module.ts
@@ -6,21 +6,26 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
+
 import { ProductListComponent } from './products/product-list.component';
+import { ProductDetailGuard } from './products/product-guard.service';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { ProductFilterPipe } from './products/product-filter.pipe';
 import { StarComponent} from './shared/star.component';
 
 @NgModule({
-  imports: [ 
+  imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-     RouterModule.forRoot([
+    RouterModule.forRoot([
       { path: 'welcome', component: WelcomeComponent },
+      { path: 'product/:id', 
+      canActivate: [ ProductDetailGuard ],
+      component: ProductDetailComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-    ])
+    ]),
   ],
   declarations: [ 
     AppComponent,
@@ -30,6 +35,7 @@ import { StarComponent} from './shared/star.component';
     ProductFilterPipe,
     StarComponent
   ],
+  providers: [ ProductDetailGuard ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- APM - Start/app/products/product-detail.component.ts
+++ APM - Start/app/products/product-detail.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 import { IProduct } from './product';
-
+import { ProductService } from './product.service';
 
 @Component({
-    moduleId: module.id,
-    templateUrl: 'product-detail.component.html'
+    templateUrl: 'app/products/product-detail.component.html'
 })
-
 export class ProductDetailComponent implements OnInit {
     pageTitle: string = 'Product Detail';
-    product: IProduct[];
+    product: IProduct;
 
-    ngOnInit(): void {
-            return;
+    constructor(private _route: ActivatedRoute,
+                private _router: Router){
+
     }
 
+    ngOnInit(): void {
+        let id = +this._route.snapshot.params['id'];
+        this.pageTitle += `: $(id)`;
+    }
+
+    onBack(): void {
+        this._router.navigate(['/products']);
+    }
 }
